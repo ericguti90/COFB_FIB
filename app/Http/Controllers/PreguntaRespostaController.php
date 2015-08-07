70bd50be8015323d4a7b9200adabeb62fbe2d3a2
ericguti.90 <ericguti.90@gmail.com>
2015-08-07 12:45:12 +0200

    api v4

--- app/Http/Controllers/PreguntaRespostaController.php
+++ app/Http/Controllers/PreguntaRespostaController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 
 // Necesitaremos el modelo Fabricante para ciertas tareas.
 use App\Votacio;
+use App\Resposta;
 
 // Necesitamos la clase Response para crear la respuesta especial con la cabecera de localización en el método Store()
 use Response;
@@ -64,7 +65,7 @@ class PreguntaRespostaController extends Controller
     public function store(Request $request, $idVotacio, $idPregunta)
     {
         // Primero comprobaremos si estamos recibiendo todos los campos.
-        if ( !$request->input('titol') || !$request->input('obligatoria'))
+        if ( !$request->input('idUsuari') || !$request->input('dataHora'))
         {
             // Se devuelve un array errors con los errores encontrados y cabecera HTTP 422 Unprocessable Entity – [Entidad improcesable] Utilizada para errores de validación.
             return response()->json(['errors'=>array(['code'=>422,'message'=>'Faltan datos necesarios para el proceso de alta.'])],422);
@@ -80,18 +81,37 @@ class PreguntaRespostaController extends Controller
             // En code podríamos indicar un código de error personalizado de nuestra aplicación si lo deseamos.
             return response()->json(['errors'=>array(['code'=>404,'message'=>'No se encuentra una votacio con ese código.'])],404);
         }
-        
-        //control de los booleanos (v1, en la v2 ira fuera)
-        if ($request->input('obligatoria') == "true") $request->merge(array('obligatoria' => true));
-        else $request->merge(array('obligatoria' => false));
 
-        // Si la votacio existe entonces lo almacenamos.
-        // Insertamos una fila en Preguntes con create pasándole todos los datos recibidos.
-        $novaPregunta=$votacio->preguntes()->create($request->all());
- 
+        $pregunta=$votacio->preguntes()->find($idPregunta);
+        if (! $pregunta)
+        {
+            // Se devuelve un array errors con los errores encontrados y cabecera HTTP 404.
+            // En code podríamos indicar un código de error personalizado de nuestra aplicación si lo deseamos.
+            return response()->json(['errors'=>array(['code'=>404,'message'=>'No se encuentra una pregunta con ese código.'])],404);
+        }
+        if ($pregunta->obligatoria == 1 && !$request->input('resposta')) {
+            return response()->json(['errors'=>array(['code'=>422,'message'=>'Falta resposta, la pregunta es obligatoria'])],422);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //queda comprovar l'usuari
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        $formato = 'Y-m-d H:i:s';
+        $fecha = DateTime::createFromFormat($formato, $request->input('dataHora'));
+        $request->merge(array('dataHora' => $fecha));
+
+        //$novaResposta=$pregunta->respostes()->create($request->all());
+        $novaResposta = New Resposta();
+        $novaResposta->resposta = $request->input('resposta');
+        $novaResposta->dataHora = $request->input('dataHora');
+        $novaResposta->votacio_id = $votacio->id;
+        $novaResposta->pregunta_id = $pregunta->id;
+        $novaResposta->usuari_id = $request->input('idUsuari');
+        $novaResposta->save();
         // Más información sobre respuestas en http://jsonapi.org/format/
         // Devolvemos el código HTTP 201 Created – [Creada] Respuesta a un POST que resulta en una creación. Debería ser combinado con un encabezado Location, apuntando a la ubicación del nuevo recurso.
-        $response = Response::make(json_encode(['data'=>$novaPregunta]), 201)->header('Location', 'http://www.dominio.local/preguntas/'.$novaPregunta->id)->header('Content-Type', 'application/json');
+        $response = Response::make(json_encode(['data'=>$novaResposta]), 201)->header('Location', 'http://www.dominio.local/respostes/'.$novaResposta->id)->header('Content-Type', 'application/json');
         return $response;
     }
 
@@ -101,9 +121,33 @@ class PreguntaRespostaController extends Controller
      * @param  int  $id
      * @return Response
      */
-    public function show($id)
+    public function show($idVotacio, $idPregunta, $idResposta)
     {
-        //
+        // Devolverá todos los assistents.
+        //return "Mostrando los aviones del fabricante con Id $idFabricante";
+        $votacio= Votacio::find($idVotacio);
+ 
+        // Si no existe la votacio que le hemos pasado mostramos otro código de error de no encontrado.
+        if (!$votacio)
+        {
+            // Se devuelve un array errors con los errores encontrados y cabecera HTTP 404.
+            // En code podríamos indicar un código de error personalizado de nuestra aplicación si lo deseamos.
+            return response()->json(['errors'=>array(['code'=>404,'message'=>'No se encuentra una votacio con ese código.'])],404);
+        }
+
+        $pregunta=$votacio->preguntes()->find($idPregunta);
+        if (! $pregunta)
+        {
+            // Se devuelve un array errors con los errores encontrados y cabecera HTTP 404.
+            // En code podríamos indicar un código de error personalizado de nuestra aplicación si lo deseamos.
+            return response()->json(['errors'=>array(['code'=>404,'message'=>'No se encuentra una pregunta con ese código.'])],404);
+        }
+        $resposta = $pregunta->respostes()->find($idResposta);
+        if (! $resposta){
+            return response()->json(['errors'=>array(['code'=>404,'message'=>'No se encuentra una resposta con ese código.'])],404);
+        }
+        return response()->json(['status'=>'ok','data'=>$resposta],200);
+        //return response()->json(['status'=>'ok','data'=>$fabricante->aviones],200);
     }
 
     /**
@@ -135,8 +179,36 @@ class PreguntaRespostaController extends Controller
      * @param  int  $id
      * @return Response
      */
-    public function destroy($id)
+    public function destroy($idVotacio, $idPregunta, $idResposta)
     {
-        //
+        // Comprobamos si la votacio que nos están pasando existe o no.
+        $votacio= Votacio::find($idVotacio);
+ 
+        // Si no existe la votacio que le hemos pasado mostramos otro código de error de no encontrado.
+        if (!$votacio)
+        {
+            // Se devuelve un array errors con los errores encontrados y cabecera HTTP 404.
+            // En code podríamos indicar un código de error personalizado de nuestra aplicación si lo deseamos.
+            return response()->json(['errors'=>array(['code'=>404,'message'=>'No se encuentra una votacio con ese código.'])],404);
+        }
+
+        $pregunta=$votacio->preguntes()->find($idPregunta);
+        if (! $pregunta)
+        {
+            // Se devuelve un array errors con los errores encontrados y cabecera HTTP 404.
+            // En code podríamos indicar un código de error personalizado de nuestra aplicación si lo deseamos.
+            return response()->json(['errors'=>array(['code'=>404,'message'=>'No se encuentra una pregunta con ese código.'])],404);
+        }
+        $resposta = $pregunta->respostes()->find($idResposta);
+        if (! $resposta){
+            return response()->json(['errors'=>array(['code'=>404,'message'=>'No se encuentra una resposta con ese código.'])],404);
+        }
+ 
+        // Procedemos por lo tanto a eliminar el assistent.
+        $resposta->delete();
+ 
+        // Se usa el código 204 No Content – [Sin Contenido] Respuesta a una petición exitosa que no devuelve un body (como una petición DELETE)
+        // Este código 204 no devuelve body así que si queremos que se vea el mensaje tendríamos que usar un código de respuesta HTTP 200.
+        return response()->json(['code'=>204,'message'=>'Se ha eliminado la pregunta correctamente.'],204);
     }
 }
